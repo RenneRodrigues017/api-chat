48f4dfcdd49c9f90700789ebea55ed12e2c4a753
RenneRodrigues <rennefrancisco9@gmail.com>
2025-10-29 13:02:46 -0300

    AjusteSwagger

--- Program.cs
+++ Program.cs
@@ -25,13 +25,12 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-
-   app.UseSwagger();
-   app.UseSwaggerUI();
-
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "APIChat v1");
+    c.RoutePrefix = "swagger"; 
+});
 
 app.UseHttpsRedirection();
 
